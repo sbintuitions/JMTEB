b71bd36131b3606b437c5b073cd3e1ead0ed2ace
shengzhe.li <shengzhe.li@sbintuitions.co.jp>
2024-04-09 16:54:43 +0900

    Use mock OpenAI embedding method in test cases instead of calling API

--- tests/embedders/test_openai.py
+++ tests/embedders/test_openai.py
@@ -1,13 +1,45 @@
-import numpy as np
+from __future__ import annotations
 
-from jmteb.embedders.openai_embedder import OpenAIEmbedder
+from dataclasses import dataclass
+
+import numpy as np
+import pytest
+from pytest_mock import MockerFixture
+
+from jmteb.embedders import OpenAIEmbedder, TextEmbedder
 
 OUTPUT_DIM = 1536
 
 
-class TestSentenceBertEmbedder:
-    def setup_class(cls):
-        cls.model = OpenAIEmbedder(model="text-embedding-3-small")
+@pytest.fixture(scope="function")
+def mock_openai_embedder(mocker: MockerFixture):
+    mocker.patch("jmteb.embedders.openai_embedder.OpenAI")
+    return OpenAIEmbedder(model="text-embedding-3-small")
+
+
+@dataclass
+class MockData:
+    data: list
+
+
+@dataclass
+class MockEmbedding:
+    embedding: list
+
+
+class MockEmbedder:
+    def create(input: str | list[str], model: str, dimensions: int):
+        if isinstance(input, str):
+            input = [input]
+        return MockData([MockEmbedding(embedding=[0.1] * dimensions)] * len(input))
+
+
+@pytest.mark.usefixtures("mock_openai_embedder")
+class TestOpenAIEmbedder:
+    @pytest.fixture(autouse=True)
+    def setup_class(cls, mocker: MockerFixture, mock_openai_embedder: TextEmbedder):
+        cls.model = mock_openai_embedder
+        cls.mock_create = mocker.patch.object(cls.model.client, "embeddings", new=MockEmbedder)
 
     def test_encode(self):
         embeddings = self.model.encode("任意のテキスト")
