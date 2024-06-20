0b0fa88b238f8009a26b12e543e48e3da85ee056
shengzhe.li <shengzhe.li@sbintuitions.co.jp>
2024-06-20 22:31:27 +0900

    Implement multi-thread encoding

--- src/jmteb/embedders/sbert_embedder.py
+++ src/jmteb/embedders/sbert_embedder.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from loguru import logger
 import numpy as np
 from sentence_transformers import SentenceTransformer
+import torch
 
 from jmteb.embedders.base import TextEmbedder
 
@@ -24,7 +26,13 @@ class SentenceBertEmbedder(TextEmbedder):
             self.model.max_seq_length = max_seq_length
 
         self.batch_size = batch_size
+        # When device is None and multi-GPU is available, do encoding with `model.encode` which is single-thread.
+        # About setting `device` with multi-thread, refer to:
+        #     https://github.com/UKPLab/sentence-transformers/blob/e5c15a51a72a5432370c4daa1d0ef7be67b4ce50/sentence_transformers/SentenceTransformer.py#L703-L706  # noqa: E501
         self.device = device
+        n_available_gpus = torch.cuda.device_count()
+        if n_available_gpus > 1:
+            self.device = [f"cuda:{i}" for i in range(n_available_gpus)]
         self.normalize_embeddings = normalize_embeddings
         self.max_seq_length = getattr(self.model, "max_seq_length", None)
         self.add_eos = add_eos
@@ -32,6 +40,16 @@ class SentenceBertEmbedder(TextEmbedder):
     def encode(self, text: str | list[str], prefix: str | None = None) -> np.ndarray:
         if self.add_eos:
             text = self._add_eos_func(text)
+        if self.device and isinstance(text, list):
+            try:
+                return self._encode_multi_process(text, prefix=prefix)
+            except Exception as e:
+                logger.error(str(e))
+                return self._encode_mono_process(text, prefix=prefix)
+        else:
+            return self._encode_mono_process(text, prefix=prefix)
+
+    def _encode_mono_process(self, text: str | list[str], prefix: str | None = None) -> np.ndarray:
         return self.model.encode(
             text,
             prompt=prefix,
@@ -40,6 +58,18 @@ class SentenceBertEmbedder(TextEmbedder):
             device=self.device,
             normalize_embeddings=self.normalize_embeddings,
         )
+
+    def _encode_multi_process(self, text: list[str], prefix: str | None = None) -> np.ndarray:
+        pool = self.model.start_multi_process_pool(target_devices=self.device)
+        embeddings = self.model.encode_multi_process(
+            sentences=text,
+            prompt=prefix,
+            batch_size=self.batch_size,
+            normalize_embeddings=self.normalize_embeddings,
+            pool=pool,
+        )
+        self.model.stop_multi_process_pool(pool)
+        return embeddings
 
     def _add_eos_func(self, text: str | list[str]) -> str | list[str]:
         try:
--- tests/embedders/test_sbert.py
+++ tests/embedders/test_sbert.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from unittest.mock import patch
+
 from jmteb.embedders.sbert_embedder import SentenceBertEmbedder
 
 MODEL_NAME_OR_PATH = "prajjwal1/bert-tiny"
@@ -22,3 +24,17 @@ class TestSentenceBertEmbedder:
         assert self.model.model.tokenizer.sep_token == "[SEP]"
         model = SentenceBertEmbedder(MODEL_NAME_OR_PATH, tokenizer_kwargs={"sep_token": "<sep>"})
         assert model.model.tokenizer.sep_token == "<sep>"
+
+    def test_multi_process(self):
+        with patch.object(SentenceBertEmbedder, "_encode_multi_process") as mock_method:
+            model = SentenceBertEmbedder(MODEL_NAME_OR_PATH, device=["cpu, cpu"])
+            model.encode(["任意のテキスト", "任意のテキスト"])
+            mock_method.assert_called_once()
+
+    def test_multi_process_invalid_when_text_is_str(self):
+        with patch.object(SentenceBertEmbedder, "_encode_multi_process") as mock_multi_method:
+            with patch.object(SentenceBertEmbedder, "_encode_mono_process") as mock_mono_method:
+                model = SentenceBertEmbedder(MODEL_NAME_OR_PATH, device=["cpu, cpu"])
+                model.encode("任意のテキスト")
+                mock_multi_method.assert_not_called()
+                mock_mono_method.assert_called_once()
