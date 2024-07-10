c446606067b9d47bfe90edd7efa51b58b1741881
shengzhe.li <shengzhe.li@sbintuitions.co.jp>
2024-07-11 05:25:19 +0900

    Fix lint

--- src/jmteb/evaluators/sts/evaluator.py
+++ src/jmteb/evaluators/sts/evaluator.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 import math
+from dataclasses import dataclass
 from os import PathLike
 from pathlib import Path
 from typing import Callable
